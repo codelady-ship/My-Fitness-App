2e51befb27416a480992a5fb7bbddfed6bf6213f
codelady-ship <jalilova.antiqa@gmail.com>
2025-03-28 18:23:04 +0400

    navbar color

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className="px-5 bg-blue-500">
-    <div className='flex justify-between h-[80px]'>
-      <Link href='/' className='font-bold text-2xl'>My Fitness App</Link>
+    <div className='flex justify-between h-[80px] text-amber-2xl '>
+      <Link href='/' className='font-bold text-amber-2xl mt-2 text'>My Fitness App</Link>
       <div className='flex items-center gap-2'>
-        <Image src="/images.jpg" alt='user-avatar' width={50} height={50}/>
+        <Image src="/images.jpg" alt='user-avatar' width={60} height={60} className='rounded-full border-3 border-b-pink-50'/>
         <p className='font-bold'>User Name</p>
       </div>
     </div>
